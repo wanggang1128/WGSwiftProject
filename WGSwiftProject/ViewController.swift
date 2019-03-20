48e0bfa6ae0d076f3aa200cf96945e393069b841
王刚 <wanggang@317hu.com>
2019-03-20 11:11:56 +0800

    Initial Commit

---
+++ WGSwiftProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WGSwiftProject
+//
+//  Created by wanggang on 2019/3/20.
+//  Copyright © 2019 bozhong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ WGSwiftProjectTests/WGSwiftProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  WGSwiftProjectTests.swift
+//  WGSwiftProjectTests
+//
+//  Created by wanggang on 2019/3/20.
+//  Copyright © 2019 bozhong. All rights reserved.
+//
+
+import XCTest
+@testable import WGSwiftProject
+
+class WGSwiftProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
