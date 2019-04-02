1cf0b32fb35a52182b9caaf7a9843c6c085cf2dc
王刚 <wanggang@317hu.com>
2019-04-02 17:00:02 +0800

    提交

--- commitGit.sh
+++ commitGit.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-
+#注释长度不为零才可提交
 if [ -z $1 ]
 then
     echo "======请填写提交注释======"
 else
-    echo "准备add"
     git add .
-    echo "准备commit"
     git commit -m $1
-    echo "准备pull"
     git pull
-    echo "准备push"
     git push origin master
     echo "======提交成功======"
 fi
