145d05458e97788b805bce092fc2e8034d782e0c
王刚 <wanggang@317hu.com>
2019-04-02 16:57:54 +0800

    shell脚本提交

--- commitGit.sh
+++ commitGit.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-echo "准备add"
-git add .
-echo "准备commit"
-git commit -m $1
-echo "准备pull"
-git pull
-echo "准备push"
-git push origin master
-echo "提交成功"
+
+if [ -z $1 ]
+then
+    echo "======请填写提交注释======"
+else
+    echo "准备add"
+    git add .
+    echo "准备commit"
+    git commit -m $1
+    echo "准备pull"
+    git pull
+    echo "准备push"
+    git push origin master
+    echo "======提交成功======"
+fi
